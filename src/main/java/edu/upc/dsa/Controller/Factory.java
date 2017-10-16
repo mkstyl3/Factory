141da566dbbd75cdace7e2c799f38b85ddeba2d5
Dani <dani.dalfo.f@gmail.com>
2017-10-16 12:15:41 +0200

    Factory semi implementada

--- src/main/java/edu/upc/dsa/Controller/Factory.java
+++ src/main/java/edu/upc/dsa/Controller/Factory.java
@@ -1,4 +1,34 @@
 package edu.upc.dsa.Controller;
 
+import edu.upc.dsa.Model.C1;
+import edu.upc.dsa.Model.C2;
+import edu.upc.dsa.Model.C3;
+
+import java.util.HashMap;
+
 public class Factory {
+
+    HashMap<String, Command> cache;
+
+    public Command getCommand(String sCMD) {
+        Command cmd = cache.get(sCMD);
+
+        if (cmd == null) {
+            cmd = loadClass(sCMD);
+            cache.put(sCMD, cmd);
+        }
+
+        return cmd;
+    }
+
+    private Command loadClass(String sCMD) {
+        Command cmd=null;
+
+        if (sCMD.equals("C1")) cmd = new C1();
+        else if (sCMD.equals("C2")) cmd = new C2();
+        else if (sCMD.equals("C3")) cmd = new C3();
+
+        return cmd;
+
+    }
 }
