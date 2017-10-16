c18b2557ebe1bed5a47d23364e4b522e35b1aa23
Dani <dani.dalfo.f@gmail.com>
2017-10-16 12:32:07 +0200

    Singleton implementat

--- src/main/java/edu/upc/dsa/Model/C3.java
+++ src/main/java/edu/upc/dsa/Model/C3.java
@@ -3,7 +3,8 @@ package edu.upc.dsa.Model;
 import edu.upc.dsa.Controller.Command;
 
 public class C3 implements Command {
-    public void execute() {
+    public int execute() {
 
+        return 3;
     }
 }
